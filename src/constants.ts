ad3af24ebfd08b7a809972f72a7c82e64f46c4d4
Diogo Kawamoto <diogo.kawamoto@gmail.com>
2025-07-21 17:09:48 -0300

    Add app secret config

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
+export const WHATSAPP_APP_SECRET = process.env.WHATSAPP_APP_SECRET || ''
 export const WHATSAPP_PASSPHRASE = process.env.WHATSAPP_PASSPHRASE || ''
 export const WHATSAPP_PRIVATE_KEY_PEM = process.env.WHATSAPP_PRIVATE_KEY_PEM || ''
