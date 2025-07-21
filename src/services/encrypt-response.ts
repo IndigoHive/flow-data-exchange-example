4d2b8529c8a0b951151b452088075b9f63e701f5
Diogo Kawamoto <diogo.kawamoto@gmail.com>
2025-07-21 15:11:04 -0300

    Add encryptResponse function

---
+++ src/services/encrypt-response.ts
@@ -0,0 +1,28 @@
+import crypto from 'node:crypto'
+
+export function encryptResponse (
+  response: any,
+  aesKeyBuffer: Buffer,
+  initialVectorBuffer: Buffer
+): string {
+  // flip initial vector
+  const flipped_iv = []
+  for (const pair of initialVectorBuffer.entries()) {
+    flipped_iv.push(~pair[1])
+  }
+
+  // encrypt response data
+  const cipher = crypto.createCipheriv(
+    'aes-128-gcm',
+    aesKeyBuffer,
+    Buffer.from(flipped_iv)
+  )
+
+  const result = Buffer.concat([
+    cipher.update(JSON.stringify(response), 'utf-8'),
+    cipher.final(),
+    cipher.getAuthTag(),
+  ]).toString('base64')
+
+  return result
+}
