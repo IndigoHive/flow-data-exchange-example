f2409784d3683614bd9040dc16b7831f3e319d52
Diogo Kawamoto <diogo.kawamoto@gmail.com>
2025-07-21 16:51:22 -0300

    Fix constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,2 @@
-export const TAG_LENGTH = 16
-export const WHATSAPP_PASSPHRASE = process.env.PASSPHRASE || ''
-export const WHATSAPP_PRIVATE_KEY_PEM = process.env.PRIVATE_KEY_PEM || ''
+export const WHATSAPP_PASSPHRASE = process.env.WHATSAPP_PASSPHRASE || ''
+export const WHATSAPP_PRIVATE_KEY_PEM = process.env.WHATSAPP_PRIVATE_KEY_PEM || ''
